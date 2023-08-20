91bab28332635fd4427555d533ed356f95978013
Enjiao Chen <erniesg@users.noreply.github.com>
2023-08-20 11:45:17 +0800

    updated utils to check for new files and added metadata

--- db/connectors.py
+++ db/connectors.py
@@ -2,18 +2,33 @@ import os
 import git
 import shutil
 from dotenv import load_dotenv, find_dotenv
-from chunk import process_python_file
 import random
+from chunks import process_python_file
 
 # Load environment variables from .env
 load_dotenv(find_dotenv())
 
+def get_repo_name_and_path(repo_url, base_path):
+    """
+    Extracts the repo name and constructs the repo path based on the provided repo URL and base path.
+    """
+    repo_name = repo_url.rstrip('/').split('/')[-1].replace('.git', '')
+    repo_path = os.path.join(os.path.dirname(os.path.abspath(base_path)), 'raw_data', 'git', repo_name)
+    return repo_name, repo_path
+
+def get_configurations():
+    """
+    Loads configuration values from environment or uses default.
+    """
+    return {
+        "DEFAULT_REPO_URL": os.getenv("DEFAULT_REPO_URL"),
+        "DEFAULT_BASE_PATH": os.getenv("DEFAULT_BASE_PATH"),
+        "GITHUB_TOKEN": os.getenv("GITHUB_TOKEN")
+    }
+
 def copy_py_files(src_dir, dest_dir):
     """
     Recursively copy .py files from source directory to destination directory.
-
-    :param src_dir: Source directory containing .py files.
-    :param dest_dir: Destination directory.
     """
     for root, dirs, files in os.walk(src_dir):
         for file in files:
@@ -24,102 +39,71 @@ def copy_py_files(src_dir, dest_dir):
                 os.makedirs(dest_dirname, exist_ok=True)
                 shutil.copy2(src_path, dest_path)
 
-def ingest_git_repo(repo_url=None, base_path=None, token=None):
+def ingest_git_repo(repo_url, base_path, token=None):
     """
-    Clone a git repository and copy only .py files into the desired structure.
-
-    :param repo_url: The URL of the git repository.
-    :param base_path: The base path where the repo will be cloned.
-    :param token: Optional GitHub personal access token for private repositories.
-    :return: The path to the cloned repository.
+    Clone a git repository, copy only .py files, and return the path to the location where they are saved.
     """
-    # Use default values from .env if not provided
-    repo_url = repo_url or os.getenv("DEFAULT_REPO_URL")
-    base_path = base_path or os.getenv("DEFAULT_BASE_PATH")
-
-    if not repo_url or not base_path:
-        raise ValueError("Repo URL and base path must be provided or set in .env")
-
-    print(f"Using repo URL: {repo_url}")
-    print(f"Using base path: {base_path}")
-
-    repo_name = repo_url.rstrip('/').split('/')[-1].replace('.git', '')
-    repo_path = os.path.join(os.path.dirname(os.path.abspath(base_path)), 'raw_data', 'git', repo_name)
-
-    print(f"Repo name: {repo_name}")
-    print(f"Repo path: {repo_path}")
-
-    if not os.path.exists(repo_path):
-        os.makedirs(repo_path)
+    repo_name, repo_path = get_repo_name_and_path(repo_url, base_path)
 
     # Clone the repo
-    if token:
-        print(f"Cloning with token")
-        git.cmd.Git().clone(repo_url, repo_path, depth=1, branch='master', config=f"http.extraheader='AUTHORIZATION: bearer {token}'")
-    else:
-        print(f"Cloning without token")
-        git.Repo.clone_from(repo_url, repo_path)
+    if not os.path.exists(repo_path):
+        os.makedirs(repo_path)
+        if token:
+            git.cmd.Git().clone(repo_url, repo_path, depth=1, branch='master', config=f"http.extraheader='AUTHORIZATION: bearer {token}'")
+        else:
+            git.Repo.clone_from(repo_url, repo_path)
 
-    # Move .py files to the commit_id directory one level up from repo_path
+    # Copy .py files to a separate directory
     commit_id = git.Repo(repo_path).head.commit.hexsha
     commit_id_directory = os.path.join(os.path.dirname(repo_path), commit_id)
     if not os.path.exists(commit_id_directory):
         os.makedirs(commit_id_directory)
-
     copy_py_files(repo_path, commit_id_directory)
 
     return commit_id_directory
 
-# Usage example:
-if __name__ == "__main__":
-    repo_url = input(f"Enter the repository URL (or press Enter to use default: {os.getenv('DEFAULT_REPO_URL')}): ") or os.getenv("DEFAULT_REPO_URL")
-    base_path = input(f"Enter the base path (or press Enter to use default: {os.getenv('DEFAULT_BASE_PATH')}): ") or os.getenv("DEFAULT_BASE_PATH")
-    token = os.getenv("GITHUB_TOKEN")  # Optional, set in .env if needed
-    repo_path = ingest_git_repo(repo_url, base_path, token)
-    print(f"Copied .py files from repository to: {repo_path}")
+def streamlined_connector_workflow():
+    config = get_configurations()
+    repo_url = input(f"Enter the repository URL (or press Enter to use default: {config['DEFAULT_REPO_URL']}): ") or config['DEFAULT_REPO_URL']
+    base_path = input(f"Enter the base path (or press Enter to use default: {config['DEFAULT_BASE_PATH']}): ") or config['DEFAULT_BASE_PATH']
+    token = config["GITHUB_TOKEN"]
 
-    # Counters for tracking
-    total_py_files_processed = 0
-    total_chunks = 0
-    all_chunks = []
+    repo_path = ingest_and_copy_py_files(repo_url, base_path, token)
+    process_files_and_print(repo_url, base_path, repo_path)
 
-    # Construct the repo path using the original logic
-    repo_name = repo_url.rstrip('/').split('/')[-1].replace('.git', '')
-    repo_path = os.path.join(os.path.dirname(os.path.abspath(base_path)), 'raw_data', 'git', repo_name)
-
-    # Initialize repo from the .git directory
+def process_files_and_print(repo_url, base_path, repo_path):
+    repo_name, _ = get_repo_name_and_path(repo_url, base_path)
     repo = git.Repo(os.path.join(repo_path, '.git'))
     commit_id = repo.head.commit.hexsha
 
-    # Process each .py file and extract chunks with metadata
-    for root, dirs, files in os.walk(repo_path):
+    all_chunks = []
+    for root, _, files in os.walk(repo_path):
         for file in files:
             if file.endswith(".py"):
-                total_py_files_processed += 1
                 file_path = os.path.join(root, file)
-
-                # Get the object ID for the file
                 object_id = repo.git.hash_object(file_path)
-
                 chunks = process_python_file(file_path, repo=repo, object_id=object_id, commit_id=commit_id)
-                total_chunks += len(chunks)
                 all_chunks.extend(chunks)
 
-    print(f"Total number of .py files processed: {total_py_files_processed}")
-    print(f"Total number of chunks generated: {total_chunks}")
-
-    # Choose a random chunk for printing
+    # Random chunk printing
     if all_chunks:
         random_chunk = random.choice(all_chunks)
-        print(f"Sample Chunk:")
-        print(f"UUID: {random_chunk['uuid']}")
-        print(f"Name: {random_chunk['name']}")
-        print(f"Code:\n{random_chunk['code']}")
-        print(f"Start Line: {random_chunk['start_line']}")
-        print(f"End Line: {random_chunk['end_line']}")
-        print(f"Parent: {random_chunk['parent']}")
-        if 'object_id' in random_chunk:
-            print(f"Object ID: {random_chunk['object_id']}")
-        if 'commit_id' in random_chunk:
-            print(f"Commit ID: {random_chunk['commit_id']}")
-        print("-" * 40)
+        print_chunk(random_chunk)
+
+def print_chunk(chunk):
+    print(f"Sample Chunk:")
+    print(f"UUID: {chunk['uuid']}")
+    print(f"Name: {chunk['name']}")
+    print(f"Code:\n{chunk['code']}")
+    print(f"Start Line: {chunk['start_line']}")
+    print(f"End Line: {chunk['end_line']}")
+    print(f"Parent: {chunk['parent']}")
+    if 'object_id' in chunk:
+        print(f"Object ID: {chunk['object_id']}")
+    if 'commit_id' in chunk:
+        print(f"Commit ID: {chunk['commit_id']}")
+    print("-" * 40)
+
+# If this script is executed directly, run the main workflow
+if __name__ == "__main__":
+    streamlined_connector_workflow()
--- db/utils.py
+++ db/utils.py
@@ -1,87 +1,85 @@
 import os
 import random
-import git
 from langchain.vectorstores import DeepLake
 from langchain.embeddings import HuggingFaceEmbeddings
-from chunk import process_python_file
-import shutil
+from chunks import process_python_file
 from dotenv import load_dotenv
-from connectors import ingest_git_repo
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path)
 active_loop_token = os.environ.get('ACTIVELOOP_TOKEN')
+random.seed()
 
-def save_to_db(repo_url, target_path, base_path, sample=False):
-    """
-    Save the entire or a sample of Git directory files to DeepLake.
+LAST_COMMIT_FILE_PATH = os.path.join(os.path.dirname(__file__), 'last_processed_commit.txt')
 
-    :param repo_url: The URL of the Git repository.
-    :param target_path: The DeepLake target path.
-    :param sample: Whether to only process a single sampled file. Default is False.
-    :return: None
-    """
+def get_last_processed_commit():
+    if not os.path.exists(LAST_COMMIT_FILE_PATH):
+        with open(LAST_COMMIT_FILE_PATH, 'w') as file:
+            file.write("")
+        return None
+
+    with open(LAST_COMMIT_FILE_PATH, 'r') as file:
+        return file.read().strip() or None
+
+def save_last_processed_commit(commit_id):
+    with open(LAST_COMMIT_FILE_PATH, 'w') as file:
+        file.write(commit_id)
+
+def save_to_db(repo_url, target_path, base_path, sample=False, commit_id=None):
     repo_name = repo_url.rstrip('/').split('/')[-1].replace('.git', '')
     repo_path = os.path.join(os.path.dirname(os.path.abspath(base_path)), 'raw_data', 'git', repo_name)
 
-    # List of file extensions to be considered
-    allowed_extensions = ['.py']  # Considering only Python files for now
+    last_commit = get_last_processed_commit()
+    if last_commit == commit_id:
+        print("No new commits found. Exiting...")
+        return
 
-    # Get all files from the repo directory
+    allowed_extensions = ['.py']
+
     all_files = []
-    for dirpath, dirnames, filenames in os.walk(repo_path):
+    for dirpath, _, filenames in os.walk(repo_path):
         for file in filenames:
-            file_extension = os.path.splitext(file)[1]
-            if file_extension in allowed_extensions:
+            if os.path.splitext(file)[1] in allowed_extensions:
                 all_files.append(os.path.join(dirpath, file))
 
-    # If sample flag is set, randomly choose a file
+    print("All files being considered:", all_files)
+    print(f"Total .py files found: {len(all_files)}")
+
     files_to_process = random.sample(all_files, 1) if sample else all_files
 
-    # Initialize DeepLake with the target path
     db = DeepLake(dataset_path=target_path, token=active_loop_token,
                   embedding_function=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2"))
 
-    # Process each .py file and extract chunks with metadata
     total_py_files_processed = 0
+    chunks_saved = 0  # Counter for the number of chunks saved
     all_documents = []
 
     for file in files_to_process:
         try:
-            # Extract code chunks using the process_python_file function
-            documents = process_python_file(file, repo=repo_path)
-
-            # Print the file type and a sample document
-            if documents:
-                doc = documents[0]  # Take the first document as a sample
-                file_type = "Valid" if isinstance(doc, dict) and 'page_content' in doc and 'metadata' in doc else "Invalid"
+            documents = process_python_file(file, repo=repo_path, commit_id=commit_id)
+            for doc in documents:
+                file_type = "Valid" if 'page_content' in doc and 'metadata' in doc else "Invalid"
                 print(f"File Type: {file_type}")
                 print("Sample Document:", doc)
-
-                # Add the documents to the list
-                all_documents.extend(documents)
-            else:
-                print("No valid documents extracted from", file)
-
+            all_documents.extend(documents)
         except Exception as e:
             print(f"Error processing file {file}: {e}")
 
         total_py_files_processed += 1
 
-    # Add the documents (chunks) to the DeepLake dataset
-    if all_documents:
-        print("Adding documents to DeepLake dataset:", all_documents)
-        # Add the documents (chunks) to the DeepLake dataset
-        for document in all_documents:
-            try:
-                page_content = document.get('page_content', '')
-                metadata = document.get('metadata', '')
+        if all_documents:
+            for document in all_documents:
+                try:
+                    page_content = document.get('page_content', '')
+                    metadata = document.get('metadata', {})
+                    if page_content:
+                        db.add_texts([page_content], metadatas=[metadata])
+                        print("Added document to DeepLake:", metadata['name'])
+                        chunks_saved += 1  # Increment the counter when chunk is saved
+                except Exception as e:
+                    print("Error adding document to DeepLake:", e)
 
-                if page_content:
-                    db.add_texts([page_content], metadatas=[metadata])
-                    print("Added document to DeepLake:", metadata['name'])
+    print(f"Saved {chunks_saved} chunks from {total_py_files_processed} files from {repo_url} to DeepLake dataset at {target_path}")
 
-            except Exception as e:
-                print("Error adding document to DeepLake:", e)
-
-    print(f"Saved chunks from {total_py_files_processed} files from {repo_url} to DeepLake dataset at {target_path}")
+    # Update the last processed commit ID after successfully processing the files.
+    save_last_processed_commit(commit_id)
