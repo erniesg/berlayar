76e33ccd67b9b7c69e5e2e5561db4a1aab35dc97
Enjiao Chen <erniesg@users.noreply.github.com>
2023-08-24 23:26:54 +0800

    added streaming to frontend and function call to email chat log to chat_adf

--- demos/chat_adf/chat.py
+++ demos/chat_adf/chat.py
@@ -1,35 +1,25 @@
 import streamlit as st
 import os
-from dotenv import load_dotenv
-from utils import Embedder
+import time
+from utils import Embedder, send_email
+import json
 
-load_dotenv()
-
-st.title("Arthur: ask me anything about your ADF")
+st.title("Bertrand - I do all the things you don't like")
 
 # Load environment variables
 user_key = os.getenv("OPENAI_API_KEY")
-# If your Embedder class requires these environment variables, load them as well
 deeplake_path = os.getenv("DEEPLAKE_PATH")
 json_path = os.getenv("JSON_PATH")
 huggingface_token = os.getenv("HUGGINGFACE_TOKEN")
 
-if user_key:
-    os.environ['OPENAI_API_KEY'] = user_key
-    st.success("OpenAI Key successfully loaded!")
-
-# If needed, set these environment variables and display their values
-if deeplake_path and json_path:
-    os.environ['DEEPLAKE_PATH'] = deeplake_path
-    os.environ['JSON_PATH'] = json_path
-    st.write("Using DeepLake path:", deeplake_path)
-    st.write("Using JSON path:", json_path)
-
-# Initialize Embedder
+# Set environment variables
+os.environ['OPENAI_API_KEY'] = user_key
+os.environ['DEEPLAKE_PATH'] = deeplake_path
+os.environ['JSON_PATH'] = json_path
 os.environ['HUGGINGFACE_TOKEN'] = huggingface_token
+
 embedder = Embedder()
 
-# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -45,9 +35,49 @@ if prompt := st.chat_input("Type your question here."):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-    # Get and display assistant response
+
+    # Get assistant response
     response = embedder.retrieve_results(prompt)
+
+    # Debugging: Print the type of the response
+    st.write(f"Type of response: {type(response)}")
+
+    if isinstance(response, str):
+        response_content = response
+    elif isinstance(response, dict) and "content" in response:
+        response_content = response["content"]
+    else:
+        response_content = ""
+
+    # Check for function calls within the response
+    if isinstance(response, dict) and "function_call" in response:
+        function_name = response["function_call"]["name"]
+        if function_name == "send_email":
+            chat_log = "\n".join([message["content"] for message in st.session_state.messages])
+            arguments = response["function_call"].get("arguments", {})
+            to_address = arguments.get("to_address", "enjiao.chen@nationalgallery.sg")
+            send_email(chat_log, to_address)
+            response_content = "Email sent successfully!"
+
+    # Display assistant's response with streaming effect
     with st.chat_message("assistant"):
-        st.markdown(response)
+        message_placeholder = st.empty()
+        full_response = ""
+        for chunk in response_content.split():
+            full_response += chunk + " "
+            time.sleep(0.1)  # Adjust this value to make the delay longer or shorter
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": response_content})
+
+# Add an email button below chat input
+if st.session_state.messages:
+    if st.button('Email Log'):
+        chat_log = "\n".join([message["content"] for message in st.session_state.messages])
+        to_address = "enjiao.chen@nationalgallery.sg"  # Change to the desired default address or get from user input
+        send_email(chat_log, to_address)
+        st.success("Email sent successfully!")
+else:
+    st.button('Email Log', disabled=True)
--- demos/chat_adf/utils.py
+++ demos/chat_adf/utils.py
@@ -12,9 +12,31 @@ from langchain.vectorstores import DeepLake
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
+import smtplib
+from email.message import EmailMessage
 from dotenv import load_dotenv
-load_dotenv()
-load_dotenv()  # Load the .env file
+from openai import ChatCompletion
+import openai
+from langchain.callbacks import get_openai_callback
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+# Load the environment variables and set the API key
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
+load_dotenv(dotenv_path)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def send_email(chat_log, to_address):
+    """Send an email containing the chat log."""
+    msg = EmailMessage()
+    msg.set_content(chat_log)
+    msg["Subject"] = "Chat Log"
+    msg["From"] = os.getenv("EMAIL_USER")
+    msg["To"] = to_address
+
+    with smtplib.SMTP(os.getenv("SMTP_SERVER"), int(os.getenv("SMTP_PORT"))) as server:
+        server.starttls()  # Upgrade the connection to encrypted TLS
+        server.login(os.getenv("EMAIL_USER"), os.getenv("EMAIL_PASSWORD"))
+        server.send_message(msg)
 
 class Embedder:
     def __init__(self) -> None:
@@ -74,16 +96,77 @@ class Embedder:
 
         self.retriever = self.db.as_retriever()
         self.retriever.search_kwargs['distance_metric'] = 'cos'
-        self.retriever.search_kwargs['fetch_k'] = 1
+        self.retriever.search_kwargs['fetch_k'] = 2
         self.retriever.search_kwargs['maximal_marginal_relevance'] = True
-        self.retriever.search_kwargs['k'] = 1
+        self.retriever.search_kwargs['k'] = 2
 
     def retrieve_results(self, query):
+        print("Starting retrieve_results function...")
         chat_history = list(self.MyQueue.queue)
-        qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0, model='gpt-3.5-turbo-16k'), chain_type="stuff", retriever=self.retriever)
-        result = qa({"question": query, "chat_history": chat_history})
-        self.add_to_queue((query, result["answer"]))
-        return result['answer']
+        print(f"Chat history: {chat_history}")
+
+        # Wrap both the ConversationalRetrievalChain and ChatCompletion API calls with the callback context manager
+        with get_openai_callback() as cb:
+            # Step 1: Use ConversationalRetrievalChain for the initial response
+            qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0, model='gpt-3.5-turbo-16k', streaming=True, callbacks=[StreamingStdOutCallbackHandler()]), chain_type="stuff", retriever=self.retriever)
+            result = qa({"question": query, "chat_history": chat_history})
+            print(f"Initial result from ConversationalRetrievalChain: {result}")
+            self.add_to_queue((query, result["answer"]))
+
+            # Check if a function call is present in the initial result
+            if "function_call" not in result:
+                response = result['answer']
+            else:
+                # Step 2: Use GPT-3.5-turbo with streaming enabled
+                functions = [
+                    {
+                        "name": "send_email",
+                        "description": "Send an email containing the chat log.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "to_address": {
+                                    "type": "string",
+                                    "description": "The recipient email address."
+                                },
+                            }
+                        }
+                    }
+                ]
+                messages = [{"role": "user", "content": query}] + [{"role": role, "content": content} for role, content in chat_history]
+                response = ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=messages,
+                    functions=functions,
+                    function_call="auto",
+                    streaming=True,
+                    callbacks=[StreamingStdOutCallbackHandler()]
+                )
+                print(f"GPT-3.5-turbo response: {response}")
+
+            # Print token and cost info after processing both API calls
+            print(f"Total Tokens: {cb.total_tokens}")
+            print(f"Prompt Tokens: {cb.prompt_tokens}")
+            print(f"Completion Tokens: {cb.completion_tokens}")
+            print(f"Total Cost (USD): ${cb.total_cost}")
+
+        # Handle different types of response
+        if isinstance(response, dict) and "function_call" in response:
+            function_name = response["function_call"]["name"]
+            # ... your function handling logic ...
+
+        elif isinstance(response, dict) and "choices" in response:
+            response = response["choices"][0]["message"]
+
+        elif isinstance(response, str):
+            # If the response is directly a string, you can leave it as it is
+            pass
+
+        else:
+            print(f"Unexpected response type: {type(response)}")
+            response = "Sorry, I couldn't understand the response."
+
+        return response
 
     def add_to_queue(self, chat):
         if self.MyQueue.full():
