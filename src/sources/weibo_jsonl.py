24da68e8c1255f715a0ba6941654ac9c064b1f4c
Chen Enjiao <erniesg@users.noreply.github.com>
2023-09-14 17:37:25 +0800

    updated gpt model and functional tests

---
+++ src/sources/weibo_jsonl.py
@@ -0,0 +1,33 @@
+# src/sources/weibo_jsonl.py
+
+from src.base_classes import DataSource
+import openai
+import json
+from src.models import gpt
+
+class WeiboJsonlSource(DataSource):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def ingest(self):
+        """Read the .jsonl file and return its content."""
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            data = [json.loads(line) for line in file]
+        return data
+
+    def translate_content(self, content):
+        # Use OpenAI SDK for translation function call
+        # This is a placeholder; you'll replace this with the actual function call
+        return gpt.translate_with_openai(content)
+
+    def extract_semantics(self, content):
+        # Use OpenAI SDK for semantic extraction function call
+        # This is another placeholder; replace this too
+        return gpt.extract_semantics_with_openai(content)
+
+    def process(self):
+        data = self.ingest()
+        for record in data:
+            translated_content = self.translate_content(record["content"])
+            semantics = self.extract_semantics(translated_content)
+            # Store the results back into the record or handle as needed
